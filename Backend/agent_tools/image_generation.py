0eab3781795b59e03ec66fc3a7bc863f2fb60547
Addyk-24 <addykatkar24@gmail.com>
2025-11-07 10:50:35 +0530

    Agent state Enhanced version

--- Backend/agent_tools/image_generation.py
+++ Backend/agent_tools/image_generation.py
@@ -1,15 +1,25 @@
 from diffusers import DiffusionPipeline
 from langchain.tools import tool
-
+import torch
 
 @tool
 def img_pipeline(query: str):
-    """  """
-    img_pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0")
-    prompt = query
+    """ Image Generating tool  """
 
-    image = img_pipe(prompt).images[0]
+    try:
+        # Load Diffusion Image Model
+        device = "cude" if torch.cude.is_available() else "cpu"
+        img_pipe = DiffusionPipeline.from_pretrained(
+            "stabilityai/stable-diffusion-xl-base-1.0",
+            torch_dtype=torch.float16 if device == "cuda" else torch.float32
+        ).to(device)        
+        
+        prompt = query
 
-    return image
+        image = img_pipe(prompt).images[0]
+
+        return image
+    except Exception as e:
+        raise ValueError(f" :( Error in Generating Image: {e} ")
 
 
